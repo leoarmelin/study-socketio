79e02635d92133afba9a0ab0810ecc84f14369cb
leoarmelin <leonardoarmelin@codeby.com.br>
2021-08-21 20:19:40 -0300

    feat(Message): create component

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,27 @@
+import { format, parseISO } from 'date-fns';
+import useSocket from '../../hooks/useSocket';
+import { IMessage } from '../../interfaces/IMessage';
+import { Container, MessageText, MessageTime } from './styles';
+
+interface IMessageComponent {
+  message: IMessage;
+}
+
+export const MessageComponent = ({ message }: IMessageComponent) => {
+  const { socket } = useSocket();
+
+  return (
+    <Container
+      isUser={socket.id === message.userId}
+      fromServer={message.fromServer}
+    >
+      <MessageText>{message.text}</MessageText>
+
+      {!message.fromServer && (
+        <MessageTime>
+          {format(parseISO(message.createdAt.toString()), "H':'mm")}
+        </MessageTime>
+      )}
+    </Container>
+  );
+};
---
+++ src/components/Message/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components';
+
+interface IContainer {
+  isUser: boolean;
+  fromServer: boolean;
+}
+
+export const Container = styled.div<IContainer>`
+  position: relative;
+
+  height: fit-content;
+
+  border-radius: 0.75rem;
+  margin-left: ${({ isUser }) => (isUser ? 'auto' : 0)};
+
+  ${({ fromServer, isUser }) =>
+    fromServer
+      ? css`
+          width: fit-content;
+          margin: 0 auto;
+          padding: 0.25rem;
+          text-align: center;
+          background-color: transparent;
+        `
+      : css`
+          width: 70%;
+          padding: 1rem;
+          background-color: ${isUser ? '#E2C0FF' : '#bafeba'};
+        `}
+`;
+
+export const MessageText = styled.p`
+  font-size: 1rem;
+`;
+
+export const MessageTime = styled.span`
+  position: absolute;
+
+  bottom: 5px;
+  right: 10px;
+
+  font-size: 0.75rem;
+`;
